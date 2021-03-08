f413a8b9fd3053191b47a631dfa902fdac13737e
SufyanIqbal1622 <bcsf16m16@gmai.com>
2021-03-08 13:45:09 +0500

    Devise added for user authentication

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
 class Project < ApplicationRecord
-  #  enum status: [:created, :designing , :develoment, :testing, :completed]
-     enum status: {inreview: 0, designing: 1, develoment: 2, testing: 3, completed: 4}
+  enum status: { inreview: 0, designing: 1, develoment: 2, testing: 3, completed: 4 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root to:'home#index'
   resources :projects
   ActiveAdmin.routes(self)
   resources :project_managers, only:[:show, :edit]
