cba5fc615b99898d931c250bd87f6e1830560269
SufyanIqbal1622 <bcsf16m16@gmai.com>
2021-03-12 01:57:39 +0500

    Project Viewas designing

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -11,4 +11,17 @@ class Feature < ApplicationRecord
     ['In review', 'in-review'],
     ['Complete', 'complete']
   ]
+
+  def badge_color
+    case status
+      when 'new'
+        'secondary'
+      when 'in-progress'
+        'info'
+      when 'in-review'
+        'warning'
+      when 'complete'
+        'success'
+    end
+  end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -4,5 +4,21 @@ class Project < ApplicationRecord
   has_many :bugs, dependent: :nullify
   has_many :features, dependent: :nullify
   belongs_to :user
-  enum status: { not_started: 0, designing: 1, develoment: 2, testing: 3, completed: 4 }
+  enum status: { not_started: 0, designing: 1, development: 2, testing: 3, completed: 4 }
+
+  def badge_color
+    case status
+      when 'not_started'
+        'secondary'
+      when 'designing'
+        'warning'
+      when 'development'
+        'info'
+      when 'testing'
+        'warning'
+      when 'completed'
+        'success'
+
+    end
+  end
 end
