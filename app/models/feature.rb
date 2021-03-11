e13a8792cbed491a3f1f8a8d0770fd561903d5ef
SufyanIqbal1622 <bcsf16m16@gmai.com>
2021-03-11 12:43:56 +0500

    added a feature  status in review

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -3,11 +3,12 @@
 class Feature < ApplicationRecord
   belongs_to :project
 
-  validates :status, inclusion: { in: ['not-started', 'in-progress', 'complete'] }
+  validates :status, inclusion: { in: ['not-started', 'in-progress', 'in-review' 'complete'] }
 
   STATUS_OPTIONS = [
     ['Not started', 'not-started'],
     ['In progress', 'in-progress'],
+    ['In review', 'in-review'],
     ['Complete', 'complete']
   ]
 end
